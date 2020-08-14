b25ad8514e775f2b5e0c3794184bb09e663cbd43
Jeffrey Cartagena <jcartagena@rodeologistics.co>
2020-08-14 14:33:39 -0500

    Removing unused imports.

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,5 +1,4 @@
 import { ADD_USER, IUser } from './action';
-import { Reducer } from 'react';
 
 interface IState {
   users: IUser[];
@@ -14,7 +13,7 @@ const initialState = {
   users: []
 };
 
-export default (state: IState = initialState, action: IAction) => {
+export default (state: IState = initialState, action: IAction): IState => {
   switch (action.type) {
     case ADD_USER:
       return { users: state.users.concat(action.payload) };
